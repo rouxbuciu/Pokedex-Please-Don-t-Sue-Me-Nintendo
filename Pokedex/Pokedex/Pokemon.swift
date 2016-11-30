690518f05dc27262f0a40ae44759cc75a25577f3
roux g. buciu <acmelabs@Gh057.local>
2016-11-30 12:30:11 -0500

    Beginning project layout

---
+++ Pokedex/Pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by roux g. buciu on 2016-11-29.
+//  Copyright © 2016 ACME Labs. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+}
